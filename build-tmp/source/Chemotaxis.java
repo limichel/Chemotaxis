2455b818abb448bd0411d9b9db71584f9d2a1c8e
unknown <mili7@697-334-Smaug.sf.k12.internal>
2014-09-22 12:13:26 -0700

    Added bacteria and food.

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Bacteria [] colony;
 Food blob;
-void setup()
+public void setup()
  {
  	size(500, 500);
  	colony = new Bacteria[100];
@@ -10,7 +26,7 @@ void setup()
  	}
  	blob = new Food((int)(Math.random() * 500), (int)(Math.random() * 500), 0);
  }   
- void draw()   
+ public void draw()   
  {    
  	background(191, 225, 255);
  	for(int i = 0; i < colony.length; i++)
@@ -36,7 +52,7 @@ void setup()
  		bactY = (int)(Math.random() * 500);
  		bactColorR = (int)(Math.random() * 256);
  	}
- 	void move()
+ 	public void move()
  	{
  		if(bactX<blob.foodX)
  		{
@@ -57,7 +73,7 @@ void setup()
  		//bactX = bactX + (int)(Math.random() * 3) - 1;
  		//bactY = bactY + (int)(Math.random() * 3) - 1;
  	}
- 	void show()
+ 	public void show()
  	{
  		noStroke();
  		fill(bactColorR, 20, 20);
@@ -77,7 +93,7 @@ void setup()
  		foodColorG = (int)(Math.random() * 100);
  		foodBye = minus;
  	}
- 	void show()
+ 	public void show()
  	{
  		noStroke();
  		//stroke(0, foodColorG, 0, 50);
@@ -91,7 +107,16 @@ void setup()
  		ellipse(foodX+5, foodY, 25, 25-foodBye);
  	}
  }
-void mousePressed()
+public void mousePressed()
 {
 	blob = new Food(mouseX, mouseY, 0);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
